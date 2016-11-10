655b6fa42548dbc87f7e8628f3c501dcaee16323
PAGuyane <pauline.perbet@guyane-parcnational.fr>
2016-11-10 15:02:38 -0300

    Add files via upload

---
+++ scripts/ecwToTif.py
@@ -0,0 +1,9 @@
+##ecw=name
+##ecwFolder=folder
+
+import os
+for ecw in os.listdir(ecwFolder):
+    if ecw.endswith(".ecw"):
+        outTif = os.path.splitext(ecw)[0]+str('.tif')
+        #processing.runalg('gdalogr:translate', ecw,100.0,False,'-9999',1,'EPSG:32622',None,False,5,4,75.0,6.0,1.0,False,1,False,None,outTif)
+        outputs_GDALOGRTRANSLATE_1=processing.runalg('gdalogr:translate', ecw,100.0,True,'-9999',1,'EPSG:32622',None,False,5,4,75.0,6.0,1.0,False,2,False,None,outTif)
